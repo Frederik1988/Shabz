37809f6660547adfd726e7b962762b9fcecf6d03
Rasmus Dolmer Andersen <rasmusdolmer@gmail.com>
2018-11-27 11:23:34 +0100

    LogController og log class added

---
+++ ShabzLock/Model/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShabzLock.Model
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        private static int NextId = 1;
+
+        public Log()
+        {
+            
+        }
+
+        public Log(string name, DateTime date, string status)
+        {
+            Id = NextId++;
+            Name = name;
+            Date = date;
+            Status = status;
+        }
+    }
+}
